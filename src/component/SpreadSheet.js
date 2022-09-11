2b6b2249ce34c9e110dffe4314beabbf6d0aab80
wnstjr <wnstjr8717@gmail.com>
2022-09-11 20:57:28 +0900

    replace react-spreadsheet -> luckysheet CDN import module

--- src/component/SpreadSheet.js
+++ src/component/SpreadSheet.js
@@ -1,7 +1,38 @@
-import Spreadsheet from "react-spreadsheet";
+import React, {Component} from 'react';
+import styles from "../css/SpreadSheet.module.css"
 
-const SpreadSheet = (props) => {
-    return <Spreadsheet columnLabels={[]} data={props.data} onChange={props.setData}/>;
-};
+class SpreadSheet extends Component {
+    componentDidMount() {
+        const luckysheet = window.luckysheet;
+        luckysheet.create({
+            container: "luckysheet",
+            title: 'sheet sample',
+            showtoolbar: false,
+            showsheetbar: false,
+            showstatisticBarConfig: {
+                count: false, // Count bar
+                view: false, // Print view
+                zoom: true // Zoom
+            },
+            enableAddRow: false,
+            // data: [{celldata: [{'r': 0, 'c': 0, v: {v: 'qwe', bg: '#61dafb'}}]}],
+            row: 5,
+            column: 10
+        });
+        document.getElementById('luckysheet_info_detail_title').remove()
+        document.getElementById('luckysheet_info_detail_update').remove()
+        document.getElementById('luckysheet_info_detail_save').remove()
+        const logo = document.getElementById('luckysheet_info_detail').getElementsByClassName('luckysheet-share-logo')
+        for (let i = 0; logo.length; i++) {
+            logo[i].remove()
+        }
+        document.getElementById('luckysheet_info_detail_input').style.textAlign = 'center'
+    }
+
+    render() {
+        return <div id="luckysheet" className={styles.sheet}></div>
+    }
+
+}
 
 export default SpreadSheet
