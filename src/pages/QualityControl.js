f57ebd78454b053df3b6ca6b4e7f918eafa3e2df
wnstjr <wnstjr8717@gmail.com>
2022-09-21 14:21:49 +0900

    npm i react-collapsible (react Accordion hide&show fileupload)

--- src/pages/QualityControl.js
+++ src/pages/QualityControl.js
@@ -1,13 +1,22 @@
 import {Fragment, useState} from "react";
 import FileUpload from "../component/FileUpload";
 import SpreadSheet from "../component/SpreadSheet";
+import Collapsible from 'react-collapsible';
+
+
+const Trigger = () => {
+    return <button/>
+
+}
 
 const QualityControl = () => {
     const [file, setFile] = useState([])
     return <Fragment>
-        <FileUpload file={file} setFile={(value) => {
-            setFile(value)
-        }}/>
+        <Collapsible trigger={<Trigger/>}>
+            <FileUpload file={file} setFile={(value) => {
+                setFile(value)
+            }}/>
+        </Collapsible>
         <SpreadSheet file={file}/>
     </Fragment>
 };
