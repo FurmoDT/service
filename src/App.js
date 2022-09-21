806abe0473bff4b4db59054cbc3fa19cfa5f94ac
wnstjr <wnstjr8717@gmail.com>
2022-09-21 13:53:51 +0900

    npm i mdb-react-ui-kit (ui bootstrap)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import QualityControl from "./pages/QualityControl";
+import Navbar from "./component/Navbar";
 
 function App() {
-    return (
-        <div className="App">
-            <QualityControl/>
-        </div>
-    );
+    return <div className="App">
+        <Navbar/>
+        <QualityControl/>
+    </div>;
 }
 
 export default App;
