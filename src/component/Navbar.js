c363a99cffb19a3d01ff100db70ef0824e87fa67
wnstjr <wnstjr8717@gmail.com>
2022-09-21 16:12:23 +0900

    set navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,11 +4,9 @@ import {
     MDBNavbar,
     MDBNavbarBrand,
     MDBNavbarToggler,
-    MDBIcon,
     MDBNavbarNav,
     MDBNavbarItem,
     MDBNavbarLink,
-    MDBBtn,
     MDBDropdown,
     MDBDropdownToggle,
     MDBDropdownMenu,
@@ -16,59 +14,31 @@ import {
     MDBCollapse,
 } from 'mdb-react-ui-kit';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import {FaBars} from "react-icons/fa";
 
 export default function Navbar() {
     const [showBasic, setShowBasic] = useState(false);
 
-    return (<MDBNavbar expand='lg' light bgColor='light'>
-            <MDBContainer fluid>
-                <MDBNavbarBrand href='#'>Brand</MDBNavbarBrand>
-
-                <MDBNavbarToggler
-                    aria-controls='navbarSupportedContent'
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'
-                    onClick={() => setShowBasic(!showBasic)}
-                >
-                    <MDBIcon icon='bars' fas/>
-                </MDBNavbarToggler>
-
-                <MDBCollapse navbar show={showBasic}>
-                    <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink active aria-current='page' href='#'>
-                                Home
-                            </MDBNavbarLink>
-                        </MDBNavbarItem>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-                        </MDBNavbarItem>
-
-                        <MDBNavbarItem>
-                            <MDBDropdown>
-                                <MDBDropdownToggle tag='a' className='nav-link'>
-                                    Dropdown
-                                </MDBDropdownToggle>
-                                <MDBDropdownMenu>
-                                    <MDBDropdownItem link>Action</MDBDropdownItem>
-                                    <MDBDropdownItem link>Another action</MDBDropdownItem>
-                                    <MDBDropdownItem link>Something else here</MDBDropdownItem>
-                                </MDBDropdownMenu>
-                            </MDBDropdown>
-                        </MDBNavbarItem>
-
-                        <MDBNavbarItem>
-                            <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                                Disabled
-                            </MDBNavbarLink>
-                        </MDBNavbarItem>
-                    </MDBNavbarNav>
-
-                    <form className='d-flex input-group w-auto'>
-                        <input type='search' className='form-control' placeholder='Type query' aria-label='Search'/>
-                        <MDBBtn color='primary'>Search</MDBBtn>
-                    </form>
-                </MDBCollapse>
-            </MDBContainer>
-        </MDBNavbar>);
+    return <MDBNavbar expand='lg' light bgColor='light'>
+        <MDBContainer fluid>
+            <MDBNavbarBrand>FurmoDT</MDBNavbarBrand>
+            <MDBNavbarToggler type='button' onClick={() => setShowBasic(!showBasic)}>
+                <FaBars/>
+            </MDBNavbarToggler>
+            <MDBCollapse navbar show={showBasic}>
+                <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+                    <MDBNavbarItem>
+                        <MDBNavbarLink active={true}>
+                            QC
+                        </MDBNavbarLink>
+                    </MDBNavbarItem>
+                    <MDBNavbarItem>
+                        <MDBNavbarLink>
+                            Setting
+                        </MDBNavbarLink>
+                    </MDBNavbarItem>
+                </MDBNavbarNav>
+            </MDBCollapse>
+        </MDBContainer>
+    </MDBNavbar>
 }
