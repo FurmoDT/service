8469243f12964b5b3ebce9a4cebf31dfce8d9fb0
wnstjr <wnstjr8717@gmail.com>
2022-10-11 01:17:04 +0900

    *SAMPLE* video player

--- src/pages/QualityControl.js
+++ src/pages/QualityControl.js
@@ -4,6 +4,7 @@ import SpreadSheet from "../component/SpreadSheet";
 import Collapsible from 'react-collapsible';
 import Guideline from "../component/Guideline";
 import {MDBBtn, MDBIcon} from 'mdb-react-ui-kit';
+import VideoPlayer from "../component/VideoPlayer";
 
 
 const QualityControl = (props) => {
@@ -15,8 +16,13 @@ const QualityControl = (props) => {
                 setFile(value)
             }}/>
             <Guideline guideline={guideline} setGuideline={setGuideline}/>
+            <MDBBtn>Video</MDBBtn>
+            <MDBBtn>Grammar Check</MDBBtn>
         </Collapsible>
-        <SpreadSheet buttonDownload={props.buttonDownload} file={file} guideline={guideline}/>
+        <div style={{flexDirection: 'row', display: 'flex'}}>
+            <VideoPlayer/>
+            <SpreadSheet buttonDownload={props.buttonDownload} file={file} guideline={guideline}/>
+        </div>
         <MDBBtn id={'btn-resize'} color={'none'} floating tag='a'>
             <MDBIcon fas icon="chevron-down" size={'2x'} color={'dark'}/>
             <MDBIcon fas icon="chevron-up" size={'2x'} color={'dark'} style={{display: 'none'}}/>
