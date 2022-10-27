750b930de1bdf7e1de73f7e00b79241a2f0ebc2f
wnstjr <wnstjr8717@gmail.com>
2022-10-28 00:56:54 +0900

    filedownload

--- src/utils/fileDownload.js
+++ src/utils/fileDownload.js
@@ -1,12 +1,12 @@
 import {toSrt} from "./srtParser";
 
 
-export const fileDownload = (data, filename) => {
+export const fileDownload = (data, file) => {
     const fileData = toSrt(data)
     const blob = new Blob([fileData], {type: "text/plain"})
     const url = URL.createObjectURL(blob)
     const link = document.createElement("a")
-    link.download = `qualified_${filename}`
+    link.download = `qualified_${file.filename}`
     link.href = url;
     link.click();
 }
