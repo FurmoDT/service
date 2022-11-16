7e0c64346e9d09ed1624bb8dfffd577e4f54f133
wnstjr <wnstjr8717@gmail.com>
2022-11-16 20:09:44 +0900

    -

--- src/component/VideoPlayer.js
+++ src/component/VideoPlayer.js
@@ -8,7 +8,7 @@ const VideoPlayer = (props) => {
             layout.current.style.display = 'flex'
         } else layout.current.style.display = 'none'
     }, [props.videoUrl])
-    return <div ref={layout} style={{width: '100%', height: '300px', display: 'none'}}>
+    return <div ref={layout} style={{width: '100%', height: '250px', display: 'none'}}>
         <ReactPlayer style={{backgroundColor: 'black'}} ref={props.player} playing={ReactPlayer.canPlay(props.videoUrl) && props.play}
                      controls={ReactPlayer.canPlay(props.videoUrl)} width={'100%'} height={'100%'}
                      url={props.videoUrl}/>
