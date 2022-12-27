e42b5d6fe17ce2aadd774c2324c2b32eb1847b7e
wnstjr <wnstjr8717@gmail.com>
2022-12-27 13:47:07 +0900

    -

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
                 <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
                     <MDBNavbarItem>
                         <MDBNavbarLink style={{cursor: "pointer"}}
-                                       href={'https://docs.google.com/presentation/d/1ufx5PEX-QEAwfSYF2Y1qrY1123BJRHduZqSwqD0V3LU/edit#slide=id.g167e2001608_0_7'}
+                                       href={'https://docs.google.com/presentation/d/1fG4wnRHlrUJz9B9T7UfV4G1ekwOAUI5afmhT161O_Y0/edit?usp=sharing '}
                                        target={'_blank'}>
                             Manual
                         </MDBNavbarLink>
